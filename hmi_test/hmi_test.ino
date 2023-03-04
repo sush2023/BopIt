55336762b9bb7cd21e32b6a20a92b84c3ac4f99c
sush2023 <69727934+sush2023@users.noreply.github.com>
2023-03-04 15:44:05 -0500

    Screen/Display updates

--- hmi_test/hmi_test.ino
+++ hmi_test/hmi_test.ino
@@ -1,5 +1,5 @@
 
-int progressbar = 0;
+int i = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -8,24 +8,28 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-//  if (progressbar < 100){
-//    progressbar += 1; 
-//    delay(100); 
-//  }
-//  else{
-//    progressbar = 0; 
-//  }
-
-//  
-  double sensorValue = analogRead(A0); 
-  sensorValue = sensorValue/10.23;
-//  Serial.println(sensorValue);
-  Serial.print("j0.val=");
-  Serial.print(sensorValue,0);
-  Serial.write(0Xff);
-  Serial.write(0Xff);
-  Serial.write(0Xff);
+//  double sensorValue = analogRead(A0); 
+//  sensorValue = sensorValue/10.23;
+//  Serial.print("j0.val=");
+//  Serial.print(sensorValue,0);
+//  Serial.write(0Xff);
+//  Serial.write(0Xff);
+//  Serial.write(0Xff);
 //  delay(1); 
+  
+  if (i==8){
+    i=0;
+    delay (2000);
+  }
+  else if(i==0){
+    delay(2000);
+  }
+
+  Serial.print("page ");
+  Serial.print(i); 
+  Serial.write(0Xff);
+  Serial.write(0Xff);
+  Serial.write(0Xff);
+  delay(200);
+  i++;
 }
