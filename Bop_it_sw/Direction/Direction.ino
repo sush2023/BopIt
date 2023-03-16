381ea07f7a8dc1a2e687baf6d825720310fc1663
sush2023 <69727934+sush2023@users.noreply.github.com>
2023-03-16 12:15:13 -0400

    screen output for direction.ino

--- Bop_it_sw/Direction/Direction.ino
+++ Bop_it_sw/Direction/Direction.ino
@@ -5,6 +5,7 @@
  *
  * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick
  */
+#include <math.h>
 
 #define VRX_PIN  A0 // Arduino pin connected to VRX pin
 #define VRY_PIN  A1 // Arduino pin connected to VRY pin
@@ -19,13 +20,19 @@ void setup() {
 
 void loop() {
   // read analog X and Y analog values
-  xValue = analogRead(VRX_PIN);
-  yValue = analogRead(VRY_PIN);
+  xValue = analogRead(VRX_PIN)-507;
+  yValue = analogRead(VRY_PIN)-511;
 
-  Serial.print("x = ");
-  Serial.print(xValue);
-  Serial.print(", y = ");
-  Serial.println(yValue);
+  double deg = atan2(yValue, xValue)* 57.295779513082320876798154814105;
+  // Serial.print("x = ");
+  // Serial.print(xValue);
+  // Serial.print(", y = ");
+  // Serial.println(yValue);
+  Serial.print("aim_0.val=");
+  Serial.print(int(deg));
+  Serial.write(0Xff);
+  Serial.write(0Xff);
+  Serial.write(0Xff);
   delay(100);
   // converts the analog value to commands
   // reset commands
