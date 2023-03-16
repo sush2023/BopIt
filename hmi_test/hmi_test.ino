834324197bb65d621e4a1c9cd8ce16fbbbfd832c
sush2023 <69727934+sush2023@users.noreply.github.com>
2023-03-16 12:07:17 -0400

    hmi_test

--- hmi_test/hmi_test.ino
+++ hmi_test/hmi_test.ino
@@ -1,10 +1,19 @@
+#include <math.h>
+#include <Arduino.h>
+
+//x=180
 
 int i = 0;
+#define VRX_PIN  A0 // Arduino pin connected to VRX pin
+#define VRY_PIN  A1
+
+int x_val = 0;
+int y_val = 0; 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  int sensorValue = analogRead(A0); 
+//  int sensorValue = analogRead(A0); 
 }
 
 void loop() {
@@ -17,19 +26,39 @@ void loop() {
 //  Serial.write(0Xff);
 //  delay(1); 
   
-  if (i==8){
-    i=0;
-    delay (2000);
-  }
-  else if(i==0){
-    delay(2000);
-  }
+//  if (i==18){
+//    i=0;
+//    delay (2000);
+//  }
+//  else if(i==0){
+//    delay(2000);
+//  }
+//
+//  Serial.print("page ");
+//  Serial.print(i); 
+//  Serial.write(0Xff);
+//  Serial.write(0Xff);
+//  Serial.write(0Xff);
+//  delay(200);
+//  i++;
 
-  Serial.print("page ");
-  Serial.print(i); 
+
+//joystick
+  int xValue = analogRead(VRX_PIN)-507;
+  int yValue = analogRead(VRY_PIN)-511;
+  double deg = atan2(yValue, xValue)* 57.295779513082320876798154814105; 
+  
+
+//  Serial.print("x = ");
+//  Serial.print(xValue);
+//  Serial.print(", y = ");
+//  Serial.println(yValue); 
+//  Serial.print("degrees");
+//  Serial.println(deg);
+  Serial.print("aim_0.val=");
+  Serial.print(int(deg));
   Serial.write(0Xff);
   Serial.write(0Xff);
   Serial.write(0Xff);
-  delay(200);
-  i++;
+  delay(100);
 }
